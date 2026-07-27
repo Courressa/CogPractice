282019aadca133034f9b41363a8da3d19dbd4633
Courressa <courressamalcolm2030@gmail.com>
2026-07-27 14:57:40 -0400

    Add classes and continuous running console

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import readline from 'readline';
 import { stdin as input, stdout as output } from 'process';
 
+const rl = readline.createInterface({ input, output });
 const exampleUsers = new Map();
 
 exampleUsers.set("admin", "admin123");
@@ -9,11 +10,9 @@ exampleUsers.set("test", "test123");
 exampleUsers.set("someUser", "someUser123");
 
 const askUser = (questionToAsk) => {
-    const rl = readline.createInterface({ input, output });
-
     return new Promise((resolve, reject) => {
         rl.question(questionToAsk, (answer) => {
-            rl.close();
+            
             resolve(answer);
         });
     });
@@ -35,19 +34,97 @@ const login = async () => {
     return "";
 };
 
+const adminDashboard = (username) => {
+    console.log(`Hi ${username}! Welcome to your admin dashboard!`);
+    console.log("What would you like to do?");
+    console.log("1. View your profile?");
+}
+
+const userDashboard = (username) => {
+    console.log(`Hi ${username}! Welcome to your user dashboard!`);
+    
+}
 
 // Entry point
 const app = async () => {
     console.log("Welcome to the bank!");
-    const loggedInUsername = await login();
+    
+    let running = true;
 
-    if (loggedInUsername === "") {
-        console.log("Invalid login credentials.");
-    } else {
-        console.log(loggedInUsername);
-        return loggedInUsername;
+    while (running) {
+        const loggedInUsername = await login();
+        if (loggedInUsername === "") {
+            console.log("Invalid login credentials.");
+        } else if (loggedInUsername === "admin") {
+            adminDashboard(loggedInUsername);
+        } else {
+            userDashboard(loggedInUsername);
+        }
+
+        const wantsToCont = await askUser("Would you like to continue? (Y/N) ");
+
+        if (wantsToCont.toUpperCase() === "N") {
+            running = false;
+            rl.close();
+        }
+    
     }
 };
+
+class User {
+    constructor(username, password) {
+        // 1. Prevent direct instantiation
+        if (new.target === User) {
+        throw new TypeError("Cannot instantiate abstract class 'User' directly.");
+        }
+
+        this.username = username;
+        this.password = password;
+        this.role = "customer";
+    }
+
+    constructor(username, password, role) {
+        // 1. Prevent direct instantiation
+        if (new.target === User) {
+        throw new TypeError("Cannot instantiate abstract class 'User' directly.");
+        }
+
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    getProfile() {
+        console.log(`${this.name} (${this.email})`);
+    }
+
+    //Abstract method simulation
+    getPermissions() {
+        throw new Error("Method 'getPermissions()' must be implemented by subclasses.");
+    }
+}
+
+class Admin extends User {
+    constructor (username, password, role) {
+        super(username, password, role);
+    }
+
+    // Implementing the abstract method - Admin CRUD
+    getPermissions() {
+        console.log('read', 'write', 'delete', 'manage_users');
+    }
+
+}
+
+class Customer extends user {
+    constructor (username, password) {
+        super(username, password);
+    }
+
+    getPermissions() {
+        console.log('read');
+    }
+}
 
 app().catch((err) => {
     console.error(err);
