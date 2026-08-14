53571c8a8f98c1aa4db43e6130760093799e7a37
Courressa <courressamalcolm2030@gmail.com>
2026-08-14 05:40:22 -0400

    Attempt terraform for frontend

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.frontend.website_endpoint
+}
+
+output "website_url" {
+  value = "http://${aws_s3_bucket_website_configuration.frontend.website_endpoint}"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.frontend.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Globally unique S3 bucket name"
+  type        = string
+  default     = "student-courressamalcolm-bank-terraform-20260814"
+}
